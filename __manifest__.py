654e4c5290b60eda2bf9e68d06fa8968444570a3
arefin_shawon <arefin.shawon07@gmail.com>
2023-10-26 14:35:40 +0600

    hide menu item user-wise (compitable with Odoo v16)

---
+++ __manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Hide Any Menu User Wise',
+    'version': '16.0.1.0.0',
+    'summary': 'Hide Any Menu Item User Wise',
+    'category': 'Themes',
+    'author': 'arefin_shawon',
+    'license': 'LGPL-3',
+    # 'maintainer': 'Hasinur Arefin Shawon',
+    # 'website': "",
+    'depends': ['base'],
+    'data': [
+        'views/res_users.xml',
+        'security/security.xml'
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
---
+++ models/res_user.py
@@ -0,0 +1,36 @@
+from odoo import models, fields, api
+
+
+class HideMenuUser(models.Model):
+    _inherit = 'res.users'
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        self.clear_caches()
+        return super(HideMenuUser, self).create(vals_list)
+
+    def write(self, vals):
+        res = super(HideMenuUser, self).write(vals)
+        for menu in self.hide_menu_ids:
+            menu.write({
+                'restrict_user_ids': [(4, self.id)]
+            })
+        self.clear_caches()
+        return res
+
+    def _get_is_admin(self):
+        for rec in self:
+            rec.is_admin = False
+            if rec.id == self.env.ref('base.user_admin').id:
+                rec.is_admin = True
+
+    hide_menu_ids = fields.Many2many('ir.ui.menu', string="Menu", store=True,
+                                     help='Select menu items that needs to be '
+                                          'hidden to this user ')
+    is_admin = fields.Boolean(compute=_get_is_admin)
+
+
+class RestrictMenu(models.Model):
+    _inherit = 'ir.ui.menu'
+
+    restrict_user_ids = fields.Many2many('res.users')
